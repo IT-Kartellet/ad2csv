67dcd355339df5185b5b06f944596a49dea0c929
Troels Liebe Bentsen <troels@it-kartellet.dk>
2012-12-14 14:23:29 +0100

    Downgrade to .Net2.0 and add support for writing to a file

--- AD2CSV/Program.cs
+++ AD2CSV/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.DirectoryServices;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -17,9 +16,16 @@ namespace AD2CSV
             var quotechar = Char.Parse(ConfigurationManager.AppSettings["QuoteChar"]);
             var quotealways = Boolean.Parse(ConfigurationManager.AppSettings["QuoteAlways"]);
 
-            var filters = ConfigurationManager.AppSettings["Filters"].Split(delimiter).ToDictionary(s => s.Split('=')[0], y => new Regex(y.Split('=')[1]));
+            var filters = new Dictionary<string, Regex>();
+            foreach (var filterstr in ConfigurationManager.AppSettings["Filters"].Split(delimiter))
+            {
+                var name = filterstr.Substring(0, filterstr.IndexOf("="));
+                var regex = new Regex(filterstr.Substring(filterstr.IndexOf("=")));
+                filters.Add(name, regex);
+            }
             var headers = ConfigurationManager.AppSettings["Headers"].Split(delimiter);
             var properties = ConfigurationManager.AppSettings["Properties"].Split(delimiter);
+            var outfile = ConfigurationManager.AppSettings["OutFile"];
 
             DirectorySearcher ds = new DirectorySearcher();
             SearchResult sr = ds.FindOne();
@@ -37,51 +43,54 @@ namespace AD2CSV
             searcher.SearchScope = SearchScope.Subtree;
             searcher.Filter = "ObjectClass=person";
 
-            Console.WriteLine(String.Join(delimiter.ToString(), headers));
-            foreach (SearchResult item in searcher.FindAll())
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(outfile, true))
             {
-                DirectoryEntry entry = item.GetDirectoryEntry();
+                file.WriteLine(String.Join(delimiter.ToString(), headers));
 
-                // Filter values and skip entry if they match
-                var skip = false;
-                foreach (var filter in filters)
+                foreach (SearchResult item in searcher.FindAll())
                 {
-                    if (entry.Properties[filter.Key].Count > 0 && !filter.Value.IsMatch(entry.Properties[filter.Key].ToString(), 0))
-                    {
-                        // DO nothing 
-                    }
-                    else
-                    {
-                        skip = true;
-                        break;
-                    }
-                }
-                if (skip) continue;
+                    DirectoryEntry entry = item.GetDirectoryEntry();
 
-                // Create line for output 
-                var line = new List<string>();
-                foreach (var name in properties)
-                {
-                    if (!String.IsNullOrEmpty(name) && entry.Properties[name].Count > 0)
+                    // Filter values and skip entry if they match
+                    var skip = false;
+                    foreach (var filter in filters)
                     {
-                        var value = entry.Properties[name][0].ToString();
-                        if (quotealways || value.IndexOf(quotechar) > -1 || value.IndexOf(delimiter) > -1)
+                        if (entry.Properties[filter.Key].Count > 0 && !filter.Value.IsMatch(entry.Properties[filter.Key].ToString(), 0))
                         {
-                            line.Add(value.Replace(quotechar.ToString(), (quotechar + quotechar).ToString()));
+                            // DO nothing 
                         }
                         else
                         {
-                            line.Add(value);
+                            skip = true;
+                            break;
                         }
                     }
-                    else
+                    if (skip) continue;
+
+                    // Create line for output 
+                    var line = new List<string>();
+                    foreach (var name in properties)
                     {
-                        line.Add("");
+                        if (!String.IsNullOrEmpty(name) && entry.Properties[name].Count > 0)
+                        {
+                            var value = entry.Properties[name][0].ToString();
+                            if (quotealways || value.IndexOf(quotechar) > -1 || value.IndexOf(delimiter) > -1)
+                            {
+                                line.Add(value.Replace(quotechar.ToString(), (quotechar + quotechar).ToString()));
+                            }
+                            else
+                            {
+                                line.Add(value);
+                            }
+                        }
+                        else
+                        {
+                            line.Add("");
+                        }
                     }
+                    file.WriteLine(String.Join(";", line.ToArray()));
                 }
-                Console.WriteLine(String.Join(";", line));
             }
-            //Console.ReadKey();
         }
     }
 }
